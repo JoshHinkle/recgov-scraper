ae0397968c62634c47ee96c66cc5ac23a90aae81
Josh Hinkle <joshhinkle@Joshs-MacBook-Air.local>
2023-03-04 13:02:47 -0800

    yeah

--- scraper.py
+++ scraper.py
@@ -1,35 +1,67 @@
 import http.client, urllib
 import json
 import os
+import collections
+import time, datetime
+
+pushover_api_token = os.environ.get('PUSHOVER_API_TOKEN', "nonesuch")
+pushover_user_key = os.environ.get('PUSHOVER_USER_KEY', "nonesuch")
+
+def send_notification(payload):
+    conn = http.client.HTTPSConnection("api.pushover.net:443")
+    conn.request("POST", "/1/messages.json",
+    urllib.parse.urlencode({
+        "token": pushover_api_token,
+        "user": pushover_user_key,
+        "message": payload,
+    }), { "Content-type": "application/x-www-form-urlencoded" })
+    conn.getresponse()
 
 conn = http.client.HTTPSConnection("www.recreation.gov")
 
-pushover_api_token = os.environ.get('PUSHOVER_API_TOKEN')
-pushover_user_key = os.environ.get('PUSHOVER_USER_KEY')
+old_happy_isles_payload = ""
+old_lyell_payload = ""
 
-payload = ""
+while True:
+    print(f"Trying in 10 seconds (currently {datetime.datetime.now()})")
+    time.sleep(10)
+    payload = ""
+    available = collections.defaultdict(list)
 
-conn.request("GET", "/api/permitinyo/445859/availability?start_date=2023-07-01&end_date=2023-07-31&commercial_acct=false", payload)
+    
+    for date in ["2023-07-01", "2023-08-01"]:
+        conn.request("GET", f"/api/permitinyo/445859/availability?start_date={date}&commercial_acct=false", payload)
+        res = conn.getresponse()
+        data = res.read()
+        data_json = json.loads(data.decode("utf-8"))["payload"]
 
-res = conn.getresponse()
-data = res.read()
-data_json = json.loads(data.decode("utf-8"))["payload"]
-
-for date, divisions in data_json.items():
-    for division_code, division in divisions.items():
-        if division_code in ["44585918", "44585922"] and division["remaining"] > 0 and not division["is_walkup"]:
-            if division_code == "44585918":
-                location = "Happy Isles"
-            elif division_code == "44585922":
-                location = "Lyell Canyon"
-            print(f"{location} has {division['remaining']} remaining permits on {date}")
+        for date, divisions in data_json.items():
+            for division_code, division in divisions.items():
+                if division_code in ["44585918", "44585922"] and division["remaining"] > 0 and not division["is_walkup"] and division["remaining"] == division["total"]:
+                    available[division_code].append((date, division["remaining"]))
 
 
-conn = http.client.HTTPSConnection("api.pushover.net:443")
-conn.request("POST", "/1/messages.json",
-  urllib.parse.urlencode({
-    "token": pushover_api_token,
-    "user": pushover_api_token,
-    "message": "hello world",
-  }), { "Content-type": "application/x-www-form-urlencoded" })
-conn.getresponse()
+    happy_isles_dates = ", ".join([f"{ent[1]} spots on {ent[0]}" for ent in available["44585918"]])
+    happy_isles_payload = f"Happy Isles Permits Available: {happy_isles_dates}"
+                
+
+    lyell_dates = ", ".join([f"{ent[1]} spots on {ent[0]}" for ent in available["44585922"]])
+    lyell_payload = f"Lyell Canyon Permits Available: {lyell_dates}"
+
+
+    if happy_isles_payload==old_happy_isles_payload:
+        if lyell_payload==old_lyell_payload:
+            continue
+
+    
+    old_lyell_payload = lyell_payload
+    old_happy_isles_payload=happy_isles_payload
+
+    for dates, payload in [(available["44585918"], happy_isles_payload), (available["44585922"], lyell_payload)]:
+        if dates: 
+            print(f"Sending notifications... {payload}")
+            send_notification(payload=payload)
+    
+    
+    
+    
